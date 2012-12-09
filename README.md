45e82d950852d4f860f29d2c7c6b3146f3344bd3
Steven Occhipinti <dev@stevenocchipinti.com>
2012-12-09 21:58:41 +1100

    Skip the lenses that couldn't be parsed
    Instead of crashing!

--- README.md
+++ README.md
@@ -9,6 +9,5 @@ It works!
 TODOs
 -----
 
-- Wrap scraper in a try block to prevent errors from crashing the app
 - Make the layout responsive?
 - Make the graph touch friendly?
--- Scraper.rb
+++ Scraper.rb
@@ -27,51 +27,61 @@ class Scraper
     doc = Nokogiri::HTML(open(url))
 
     lenses = []
+    stats = {:parsed => 0, :failed => 0}
     doc.xpath('//div[contains(text(), "$")]').each do |price_element|
 
-      containing_element = price_element.xpath("ancestor::table[1]")
-      name_element = containing_element.css('.highlight')
-      image_element = containing_element.xpath("ancestor::table[1]//img")
+      begin
 
-      lens = {
-        name:          name_element.text,
-        link:          DWI_URL + name_element.first.attribute('href').text,
-        image:         DWI_URL + image_element.first.attribute('src').text,
-        aperture:      name_element.text[/\bf\/?[0-9.-]+/i],
-        focal_length:  name_element.text[/[0-9.-]+mm/i],
-        price:         price_element.text[/\$[0-9.]+/]
-      }
+        containing_element = price_element.xpath("ancestor::table[1]")
+        name_element = containing_element.css('.highlight')
+        image_element = containing_element.xpath("ancestor::table[1]//img")
 
-      # Only care about proper lenses (not teleconverters, etc.)
-      next if !lens[:aperture] || !lens[:focal_length]
+        lens = {
+          name:          name_element.text,
+          link:          DWI_URL + name_element.first.attribute('href').text,
+          image:         DWI_URL + image_element.first.attribute('src').text,
+          aperture:      name_element.text[/\bf\/?[0-9.-]+/i],
+          focal_length:  name_element.text[/[0-9.-]+mm/i],
+          price:         price_element.text[/\$[0-9.]+/]
+        }
 
-      # Find minimum and maximum aperture
-      parts = lens[:aperture].scan(/f([0-9.]+)-([0-9.]+)/i).flatten
-      if parts.any?
-        lens[:aperture_min] = parts.first
-        lens[:aperture_max] = parts.last
-      elsif aperture = lens[:aperture][/[0-9.]+/]
-        lens[:aperture_min] = lens[:aperture_max] = aperture
+        # Only care about proper lenses (not teleconverters, etc.)
+        next if !lens[:aperture] || !lens[:focal_length]
+
+        # Find minimum and maximum aperture
+        parts = lens[:aperture].scan(/f([0-9.]+)-([0-9.]+)/i).flatten
+        if parts.any?
+          lens[:aperture_min] = parts.first
+          lens[:aperture_max] = parts.last
+        elsif aperture = lens[:aperture][/[0-9.]+/]
+          lens[:aperture_min] = lens[:aperture_max] = aperture
+        end
+
+        # Find minimum and maximum focal length
+        parts = lens[:focal_length].scan(/([0-9.]+)-([0-9.]+)mm/i).flatten
+        if parts.any?
+          lens[:focal_length_min] = parts.first
+          lens[:focal_length_max] = parts.last
+        elsif focal_length = lens[:focal_length][/[0-9.]+/]
+          lens[:focal_length_min] = lens[:focal_length_max] = focal_length
+        end
+
+        # Generate coodinates of points to plot on a chart
+        min = [lens[:focal_length_min], lens[:aperture_min] ]
+        max = [lens[:focal_length_max], lens[:aperture_max] ]
+        lens[:plot] = min == max ? [min] : [min, max]
+
+        lenses << lens
+        stats[:parsed] += 1
+
+      rescue
+        stats[:failed] += 1
+        next
       end
-
-      # Find minimum and maximum focal length
-      parts = lens[:focal_length].scan(/([0-9.]+)-([0-9.]+)mm/i).flatten
-      if parts.any?
-        lens[:focal_length_min] = parts.first
-        lens[:focal_length_max] = parts.last
-      elsif focal_length = lens[:focal_length][/[0-9.]+/]
-        lens[:focal_length_min] = lens[:focal_length_max] = focal_length
-      end
-
-      # Generate coodinates of points to plot on a chart
-      min = [lens[:focal_length_min], lens[:aperture_min] ]
-      max = [lens[:focal_length_max], lens[:aperture_max] ]
-      lens[:plot] = min == max ? [min] : [min, max]
-
-      lenses << lens
 
     end
 
+    $stderr.puts "Could not parse #{stats[:failed]} lenses!" if stats[:failed] > 0
     lenses
   end
 
