a89db4777f3296e8812430f13f68c198d8b0cb1f
Steven Occhipinti <dev@stevenocchipinti.com>
2012-12-15 10:12:37 +1100

    Further refactor the Lens class

--- Lens.rb
+++ Lens.rb
@@ -1,20 +1,27 @@
+#
+# This class should be instatiated with a :name, :price, :link and :image
+#
+# Hash representation:
+# lens.to_json #=> {
+#   "aperture": "F4-5.6",
+#   "aperture_max": "5.6",
+#   "aperture_min": "4",
+#   "focal_length": "10-20mm",
+#   "focal_length_max": "20",
+#   "focal_length_min": "10",
+#   "image": "...",
+#   "link": "...",
+#   "name": "Sigma 10-20mm F4-5.6 EX DC HSM Lenses",
+#   "plot": [ ["10","4"], ["20","5.6"] ],
+#   "price": "$411.00"
+# }
+#
+
 class Lens
 
   def initialize(attrs)
     @lens = attrs
-    @lens.merge!({
-      aperture:     attrs[:name][/\bf\/?[0-9.-]+/i],
-      focal_length: attrs[:name][/[0-9.-]+mm/i],
-      price:        attrs[:price][/\$[0-9.]+/]
-    })
-
-    # Only care about proper lenses (not teleconverters, etc.)
-    if !@lens[:focal_length] || !@lens[:aperture]
-      raise "Must have a focal length and an aperture"
-    end
-
-    split_focal_length!
-    split_aperture!
+    parse_name_and_price!
     generate_plot!
   end
 
@@ -27,14 +34,20 @@ class Lens
   private
 
 
-  # Find minimum and maximum aperture
-  def split_aperture!
+  # Extract useful data from the name
+  def parse_name_and_price!
+    @lens.merge!({
+      aperture:     @lens[:name][/\bf\/?[0-9.-]+/i],
+      focal_length: @lens[:name][/[0-9.-]+mm/i],
+      price:        @lens[:price][/\$[0-9.]+/]
+    })
+
+    # Only care about proper lenses (not teleconverters, etc.)
+    if !@lens[:focal_length] || !@lens[:aperture]
+      raise "Must have a focal length and an aperture"
+    end
+
     split_range!(:aperture, /f([0-9.]+)-([0-9.]+)/i)
-  end
-
-
-  # Find minimum and maximum focal length
-  def split_focal_length!
     split_range!(:focal_length, /([0-9.]+)-([0-9.]+)mm/i)
   end
 
